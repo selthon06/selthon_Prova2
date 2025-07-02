f6626c34fbf30fbbacbf3f54d6e51e35803b8740
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2025-07-01 22:04:00 -0400

    Create ex1.py
    prova exe 1

---
+++ ex1.py
@@ -0,0 +1,28 @@
+def criar_dicionario(admin, operacional, tecnico):
+    return {
+        'Administrativo': tuple(admin),
+        'Operacional': tuple(operacional),
+        'Técnico': tuple(tecnico)
+    }
+
+def setores_com_mais_de_dois(dicionario):
+    for setor, nomes in dicionario.items():
+        if len(nomes) > 2:
+            print(f"{setor}: {len(nomes)} participantes")
+
+def exibir_nomes_ordenados(dicionario):
+    todos_nomes = []
+    for nomes in dicionario.values():
+        todos_nomes.extend(nomes)
+    todos_nomes.sort()
+    print("Nomes em ordem alfabética:")
+    for nome in todos_nomes:
+        print(nome)
+
+admin = ['Ana', 'Carlos', 'Bia']
+operacional = ['selthon', 'Rita']
+tecnico = ['Lucas', 'Tiago', 'Fernanda', 'Rafael']
+
+d = criar_dicionario(admin, operacional, tecnico)
+setores_com_mais_de_dois(d)
+exibir_nomes_ordenados(d)
